06d80393304eb3e7f725c925b5b4e7fd453ce76c
Ihor Tarkhan <ihor.tarkhan@knu.ua>
2022-09-23 23:44:48 +0300

    add prettier to frontend

--- backend/src/main/java/ua/knu/naturereserve/Application.java
+++ backend/src/main/java/ua/knu/naturereserve/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NatureReserveApplication {
+public class Application {
   public static void main(String[] args) {
-    SpringApplication.run(NatureReserveApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }

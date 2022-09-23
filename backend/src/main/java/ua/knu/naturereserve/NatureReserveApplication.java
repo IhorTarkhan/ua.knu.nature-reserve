63e733f56246e94b5c62c0250c520661fb95589f
Ihor Tarkhan <ihor.tarkhan@knu.ua>
2022-09-23 23:19:27 +0300

    first commit

---
+++ backend/src/main/java/ua/knu/naturereserve/NatureReserveApplication.java
@@ -0,0 +1,11 @@
+package ua.knu.naturereserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatureReserveApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(NatureReserveApplication.class, args);
+  }
+}
---
+++ backend/src/test/java/ua/knu/naturereserve/NatureReserveApplicationTests.java
@@ -0,0 +1,10 @@
+package ua.knu.naturereserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatureReserveApplicationTests {
+  @Test
+  void contextLoads() {}
+}

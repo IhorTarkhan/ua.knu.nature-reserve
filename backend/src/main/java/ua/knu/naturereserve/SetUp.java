e88753093d748212c1c9cc3602a67632326dd681
Ihor Tarkhan <ihor.tarkhan@knu.ua>
2022-10-06 13:00:37 +0300

    migration

---
+++ backend/src/main/java/ua/knu/naturereserve/SetUp.java
@@ -0,0 +1,29 @@
+package ua.knu.naturereserve;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import ua.knu.naturereserve.entity.Admin;
+import ua.knu.naturereserve.repository.AdminRepository;
+
+import javax.annotation.PostConstruct;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class SetUp {
+  private final AdminRepository adminRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  @PostConstruct
+  public void setUp() {
+    Optional<Admin> admin = adminRepository.findByUsername("my_admin");
+    if (admin.isEmpty()) {
+      adminRepository.save(
+          Admin.builder()
+              .username("my_admin")
+              .password(passwordEncoder.encode("password"))
+              .build());
+    }
+  }
+}

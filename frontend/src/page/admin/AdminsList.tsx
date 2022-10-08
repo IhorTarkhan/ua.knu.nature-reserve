cebc956bdb0fa084a71d68d702cdbb8a7134c18a
Ihor Tarkhan <ihor.tarkhan@knu.ua>
2022-10-08 14:38:10 +0300

    popup

--- frontend/src/page/admin/AdminsList.tsx
+++ frontend/src/page/admin/AdminsList.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ReactElement, useEffect, useState } from "react";
+import { ReactElement, useEffect, useRef, useState } from "react";
 import { Header } from "../../component/Header";
 import { navigation } from "../../constant/navigation";
 import { AdminInfo } from "../../dto/response/AdminInfo";
@@ -20,6 +20,14 @@ import IconButton from "@mui/material/IconButton";
 import BlockIcon from "@mui/icons-material/Block";
 import Tooltip from "@mui/material/Tooltip";
 import FaceIcon from "@mui/icons-material/Face";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
 
 const AdminTableHead = (): ReactElement => {
   return (
@@ -39,9 +47,14 @@ export const AdminsList = (): ReactElement => {
     { label: "Pricing", location: "/admin/pricing" },
     { label: "Blog", location: "/admin/blog" },
   ];
+  const popupPasswordRef = useRef("popup-password");
 
   const [currentAdmin, setCurrentAdmin] = useState<AdminInfo | null>(null);
   const [admins, setAdmins] = useState<AdminInfo[]>([]);
+  const [isPopup, setIsPopup] = useState<boolean>(false);
+  const [popupTitle, setPopupTitle] = useState<string>("");
+  const [popupContent, setPopupContent] = useState<ReactElement>(<></>);
+  const [popupAccept, setPopupAccept] = useState<() => void>(() => {});
 
   useEffect(() => {
     axios
@@ -53,6 +66,74 @@ export const AdminsList = (): ReactElement => {
       .then((r: AxiosResponse<AdminInfo[]>) => setAdmins(r.data))
       .catch(alert);
   }, []);
+
+  const openPopup = () => {
+    setIsPopup(true);
+  };
+
+  const handleChangeAdminPassword = (admin: AdminInfo) => {
+    setPopupTitle(
+      `Do you want to change password to admin "${admin.username} (${admin.id})"?`
+    );
+    setPopupContent(
+      <>
+        <TextField
+          id={"popup-new-password"}
+          label={"New password"}
+          type={"password"}
+          style={{ width: "100%", marginTop: 10 }}
+          inputRef={popupPasswordRef}
+        />
+      </>
+    );
+    setPopupAccept(() => () => {
+      // @ts-ignore
+      const newPassword = popupPasswordRef.current.value;
+      console.log("newPassword = " + newPassword);
+      axios
+        .get(api.HOST + api.admin.getAll)
+        .then((r: AxiosResponse<AdminInfo[]>) => setAdmins(r.data))
+        .catch(alert);
+    });
+    openPopup();
+  };
+
+  const handleDeactivateAdmin = (admin: AdminInfo) => {
+    setPopupTitle(
+      `Do you want to deactivate admin "${admin.username} (${admin.id})"?`
+    );
+    setPopupContent(<></>);
+    setPopupAccept(() => () => {
+      axios
+        .get(api.HOST + api.admin.getAll)
+        .then((r: AxiosResponse<AdminInfo[]>) => setAdmins(r.data))
+        .catch(alert);
+    });
+    openPopup();
+  };
+
+  const handleReactivateAdmin = (admin: AdminInfo) => {
+    setPopupTitle(
+      `Do you want to reactivate admin "${admin.username} (${admin.id})"?`
+    );
+    setPopupContent(<></>);
+    setPopupAccept(() => () => {
+      axios
+        .get(api.HOST + api.admin.getAll)
+        .then((r: AxiosResponse<AdminInfo[]>) => setAdmins(r.data))
+        .catch(alert);
+    });
+    openPopup();
+  };
+
+  const handleRejectPopup = () => {
+    setIsPopup(false);
+  };
+
+  const handleAcceptPopup = () => {
+    setIsPopup(false);
+    popupAccept();
+  };
 
   return (
     <>
@@ -73,12 +154,14 @@ export const AdminsList = (): ReactElement => {
                     <TableCell>{row.username}</TableCell>
                     <TableCell align={"right"}>
                       <Tooltip title={"Reset password"}>
-                        <IconButton onClick={() => alert("password")}>
+                        <IconButton
+                          onClick={() => handleChangeAdminPassword(row)}
+                        >
                           <KeyIcon fontSize={"large"} />
                         </IconButton>
                       </Tooltip>
                       <Tooltip title={"Deactivate admin"}>
-                        <IconButton onClick={() => alert("deactivate")}>
+                        <IconButton onClick={() => handleDeactivateAdmin(row)}>
                           <BlockIcon fontSize={"large"} />
                         </IconButton>
                       </Tooltip>
@@ -104,7 +187,7 @@ export const AdminsList = (): ReactElement => {
                     <TableCell>{row.username}</TableCell>
                     <TableCell align={"right"}>
                       <Tooltip title={"Activate admin"}>
-                        <IconButton onClick={() => alert("password")}>
+                        <IconButton onClick={() => handleReactivateAdmin(row)}>
                           <FaceIcon fontSize={"large"} />
                         </IconButton>
                       </Tooltip>
@@ -115,6 +198,17 @@ export const AdminsList = (): ReactElement => {
           </Table>
         </TableContainer>
       </Container>
+
+      <Dialog open={isPopup} onClose={handleRejectPopup}>
+        <DialogTitle>{popupTitle}</DialogTitle>
+        <DialogContent>{popupContent}</DialogContent>
+        <DialogActions>
+          <Button onClick={handleRejectPopup}>Reject</Button>
+          <Button onClick={handleAcceptPopup} autoFocus>
+            Accept
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };

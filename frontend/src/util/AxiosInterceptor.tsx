92f10b6c4223f281cb64cc18002823646186c572
vladkorosko <vlad.korosko1@gmail.com>
2022-10-16 19:43:59 +0300

    refactoring frontend

--- frontend/src/util/AxiosInterceptor.tsx
+++ frontend/src/util/AxiosInterceptor.tsx
@@ -3,8 +3,8 @@ export const axios = require("axios");
 type Request = { headers: any; url: string };
 
 function setAuthorizationHeaderIfNeeded(req: Request, key: string) {
-  const pathname = new URL(req.url).pathname;
-  if (pathname.startsWith(`/${key}`) && pathname !== `/${key}/login`) {
+  const path = new URL(req.url).pathname;
+  if (path.startsWith(`/${key}`) && path !== `/${key}/authorisation/login`) {
     const value = "Authorization-" + key.charAt(0).toUpperCase() + key.slice(1);
     const clientHeader: string | null = localStorage.getItem(value);
     if (clientHeader) {
